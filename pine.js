0e999705f128d21eacf86d752927535941a5600c
Timur1989geo <tusmanov1989@gmail.com>
2025-04-01 02:27:57 +0300

    Add files via upload

---
+++ pine.js
@@ -0,0 +1,8 @@
+export default function Product() {
+  return (
+    <div>
+      <h1>Пихтовое мыло</h1>
+      <p>Описание пихтовое мыло будет добавлено позже.</p>
+    </div>
+  );
+}
---
+++ tar.js
@@ -0,0 +1,8 @@
+export default function Product() {
+  return (
+    <div>
+      <h1>Дегтярное мыло</h1>
+      <p>Описание дегтярное мыло будет добавлено позже.</p>
+    </div>
+  );
+}
